28cbee1abb9b4e54a9c02ae750c85e885e7492b9
hastgt <44572610+hastgt@users.noreply.github.com>
2022-02-08 17:20:07 +0100

    Add files via upload

--- Assets/Scripts/Items/ItemManager.cs
+++ Assets/Scripts/Items/ItemManager.cs
@@ -2,12 +2,12 @@ using UnityEngine;
 
 public class ItemManager : MonoBehaviour
 {
-    private readonly string _jsonFileUrl = "https://github.com/hastgt/JSON-parcer-unity/blob/main/JSONExample.json";
+    private readonly string _jsonFileUrl = "https://raw.githubusercontent.com/hastgt/JSON-parser-unity/main/JSONExample.json";
 
     private int _timeToShowItem;
 
     private readonly JSONParser<ChestData> _jsonParser = new JSONParser<ChestData>();
-    private ChestData _chestData = new ChestData();
+    [field: SerializeField]  private ChestData _chestData = new ChestData();
 
 
     private void Start()
